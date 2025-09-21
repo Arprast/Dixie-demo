93c2f7668e3a0be7f72740a4bb1ced83db8609a6
ghivanva-cell <ghivanva@gmail.com>
2025-09-21 18:20:12 +0700

    V.0.6

---
+++ Admin/Menu.php
@@ -0,0 +1,9 @@
+<div id="sidebar-nav" class="sidebar">
+			<div class="sidebar-scroll">
+				<nav>
+					<ul class="nav">
+						<!-- ini menu -->
+					</ul>
+				</nav>
+			</div>
+		</div>
---
+++ Admin/index.php
@@ -0,0 +1,33 @@
+<?php include __DIR__. '/Head.php'; ?>
+
+<?php include __DIR__. '/Navbar.php'; ?>
+
+<?php include __DIR__. '/Menu.php'; ?>
+
+<div class="main">
+	<div class="main-content">
+		<div class="panel panel-headline">
+			<div class="panel-heading">
+				<h1 class="panel-title">ini judul</h1>
+				<p class="panel-subtitle">ini sub judul</p>
+			</div>
+			<div class="panel-body">
+				<div class="col-md-9">
+					<div id="demo-bar-chart" class="ct-chart"></div>
+				</div>
+			</div>
+			<div class="panel-footer">
+				<div class="right">
+					<button class="btn btn-primray">Kirim</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="clearfix"></div>
+
+<?php include __DIR__. '/Foot.php'; ?>
+	
+		
+		
+		
